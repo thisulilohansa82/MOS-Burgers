af5ea1024517a2d0e7ad5f85da840b279f58e093
thisulilohansa82 <thisulilohansa082@gmail.com>
2024-12-05 19:54:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let user=[
+{
+    userName:"saman",
+    password:1234
+},
+{
+    
+    userName:"kamal",
+    password:1234
+}
+];
+
+
+console.log(user);
+
+
+function login(){
+    console.log("login");
+
+    const loginUsername = document.getElementById("login-usrename").value;
+    const loginPassword = document.getElementById("login-password").value;
+}
+   
